cf5c014f1e907318466e090fd53a274385a409af
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-02 16:53:06 -0600

    Commits welcome message outline

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -4,6 +4,37 @@ class Battleship
   attr_reader   :start_time
   def initialize
     @start_time = Time.now
+    welcome
+  end
+
+  def welcome
+    puts File.read("./lib/welcome_message.txt")
+    welcome_menu
+  end
+
+  def welcome_menu
+    user_input = gets.chomp
+    if user_input.downcase == "p" || "play"
+      game_play
+    elsif user_input.downcase == "i" ||  "instructions"
+      puts File.read("./lib/instruction_message.txt")
+      welcome
+    elsif user_input.downcase == "q" || "quit"
+      end_game
+    else
+      puts "Try again Dave"
+      welcome
+    end
+  end
+
+  def game_play
+
+  end
+
+  def end_game
+
   end
 
 end
+
+intrepid = Battleship.new
