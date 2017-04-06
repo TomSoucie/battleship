602a137bbd2dea7936ba21d2bae61b13aaeb847c
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-06 07:22:28 -0600

    Commits pseudo fire_module

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
     @mac_shots    = Array.new(4) { Array.new(4)}
     @row_selector = Random.new
     @col_selector = Random.new
-    @two_s_ship   = ""
+    # @two_s_ship   = ""
   end
 
   
@@ -32,6 +32,7 @@ class Game
   end
 
   def get_input
+    puts "I have laid out my ships on the grid, now you need to layout yours"
     puts "Enter the coordinates for your first ship, which takes 2 spaces"
     puts "Enter the coordinates seperated by a space - eg 'A1 A2' "
     @user_input = gets.chomp.split(" ")
@@ -40,7 +41,7 @@ class Game
       ((@user_input[0][0].unpack('c')[0] - @user_input[1][0].unpack('c')[0]).abs == 1 && 
       (@user_input[0][1] == @user_input[1][1]))
       # binding.pry
-      make_the_grid
+      make_the_grid(@user_input)
     else   
       if (@user_input[0][0] != @user_input[1][0]) && (@user_input[0][1] != @user_input[1][1])
         puts "That's diagonal, vertical position only is allowed. Try again!"
@@ -52,8 +53,8 @@ class Game
     end
   end
 
-  def make_the_grid
-    @user_input.each do |coord|
+  def make_the_grid(input)
+    input.each do |coord|
       if coord[0] == "A"
         @player_grid[0][(coord[1].to_i)-1] = "X"
       elsif coord[0] == "B"
@@ -64,16 +65,17 @@ class Game
         @player_grid[3][(coord[1].to_i)-1] = "X"
       end 
     end     
-    display_the_grid
+    display_the_grid(@player_grid)
   end
 
-  def display_the_grid
+  def display_the_grid(grid)
     puts "---------------------------\n" * 2
     format = '%-3s %-6s %-6s %-6s %-6s'
     puts format % [".", "1", "2", "3", "4"]
-    @player_grid.each_with_index do |member, i|
-      puts format % [ (i+65).chr, @player_grid[i][0].to_s, @player_grid[i][1].to_s, @player_grid[i][2].to_s, @player_grid[i][3].to_s]
+    grid.each_with_index do |member, i|
+      puts format % [ (i+65).chr, grid[i][0].to_s, grid[i][1].to_s, grid[i][2].to_s, grid[i][3].to_s]
     end
     puts "---------------------------\n" * 2
+    # fire_sequence
   end
 end
